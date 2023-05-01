065d165fc259f51223ae3e4c8b5f2f0baae513a7
Red <red.sarouf2014@gmail.com>
2023-04-30 23:42:05 -0400

    Add intro to javascript Context

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+function createEmployeeRecord(employeeArray) {
+    return {
+      firstName: employeeArray[0],
+      familyName: employeeArray[1],
+      title: employeeArray[2],
+      payPerHour: employeeArray[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  function createEmployeeRecords(employeeRecord) {
+    return employeeRecord.map(function (record) {
+      return createEmployeeRecord(record);
+    });
+  }
+  
+  function createTimeInEvent(empRecord, date) {
+    const newTimeInEvent = {
+      type: "TimeIn",
+      hour: parseInt(date.slice(-4)),
+      date: date.slice(0, 10)
+    }
+    empRecord.timeInEvents.push(newTimeInEvent)
+    return empRecord
+  }
+  
+  function createTimeOutEvent(empRecord, date) {
+    const newTimeOutEvent = {
+      type: "TimeOut",
+      hour: parseInt(date.slice(-4)),
+      date: date.slice(0, 10)
+    }
+    empRecord.timeOutEvents.push(newTimeOutEvent)
+    return empRecord
+  }
+  
+  function hoursWorkedOnDate(empRecord, date) {
+    const timeIn = empRecord.timeInEvents.find(event => event.date === date);
+    const timeOut = empRecord.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour)/100;
+  }
+  
+  function wagesEarnedOnDate(empRecord, date) {
+    return hoursWorkedOnDate(empRecord, date) * empRecord.payPerHour;
+  }
+  
+  function calculatePayroll(empRecords) {
+    const totalForEachEmployee = empRecords.map(record => allWagesFor(record))
+    return totalForEachEmployee.reduce((total, empTotal) => total + empTotal)
+  }
+  let findEmployeeByFirstName = function (srcArray, firstName) {
+    return srcArray.find(function (rec) {
+      return rec.firstName === firstName;
+    });
+  };
+  
+  function allWagesFor(obj){
+    const allWages = obj.timeInEvents.map((day) => {return wagesEarnedOnDate(obj, day.date)})
+    return allWages.reduce((acc, cv) => acc + cv)
+}
